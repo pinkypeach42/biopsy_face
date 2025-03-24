2bc712c5430cca7802232c8ad248204ee62075e2
pinkypeach42 <elagina_daria@gmx.de>
2025-03-24 19:51:20 +0100

    before bug

--- face_detection.py
+++ face_detection.py
@@ -30,14 +30,20 @@ for i in range (len(pictures)):
      image = Image.frombytes(heif_pic.mode, heif_pic.size, heif_pic.data)
      image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
+     # save the .heic as .png, delete the old one 
+     new_name = os.path.splitext(pictures[i])[0] + ".jpg"
+     new_path = os.path.join("image", new_name)
+     cv2.imwrite(new_path, image_cv)
+     os.remove(full_path)
 
-     face = face_recognition.face_locations(image_cv, model="cnn")
+
+     '''face = face_recognition.face_locations(image_cv, model="cnn")
      print(f"Gesichtserkennung abgeschlossen. {len(face)} Gesichter gefunden.")
 
      if not face:
         print(f"Picture {pictures[i]:s} muss manuell verarbeitet werden!")
 
-     '''
+     
      start = time.time()
      
      # Jedes erkannte Gesicht ausschneiden und skalieren
@@ -64,7 +70,9 @@ for i in range (len(pictures)):
    
 
     else:
-       
-      sys.exit(0) 
+       continue
+      #sys.exit(0) 
 
 
+pictures = os.listdir("image")
+print(f"Pics: {pictures}")
