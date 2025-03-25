1b314bbf24b2dd3ef260d950ce776629d21c183e
pinkypeach42 <elagina_daria@gmx.de>
2025-03-25 20:21:41 +0100

    mal schaun mal schaun ob die schese klappt

---
+++ main.py
@@ -0,0 +1,36 @@
+#from face_detecton
+
+import tkinter as tk
+from tkinter import filedialog
+import cv2
+import os
+
+# participant_folder  - PATH of participant Folder
+# folder_content - ARRAY with all Data in Participant folder (NOT A PATH!)
+
+
+def main():
+   
+    root = tk.Tk()
+    root.withdraw()
+
+    participant_folder = filedialog.askdirectory(title="Wähle ein Probanden-Verzeichnis aus")
+
+    print("Chosen folder:", participant_folder)
+
+    # ARRAY with all Data (folders, pictures), NOT A PATH ANYMORE!
+    folder_content = os.listdir(participant_folder) 
+    print (folder_content)
+
+    for item in folder_content:
+        full_path = os.path.join(participant_folder, item)
+        if os.path.isdir(full_path):
+        
+         print (f'Folder "{item}" found: {full_path}')
+         images = os.listdir(full_path)
+
+        #print(images) #test the content of the folder
+
+
+if __name__ == "__main__":
+    main()
