604be2e69bd7c0cc3615da42da22217d4a5cfc54
pinkypeach42 <elagina_daria@gmx.de>
2025-03-25 22:17:42 +0100

    fertig

--- face_detection.py
+++ face_detection.py
@@ -33,6 +33,75 @@ def align_format (folder_content, full_path):
        os.remove(os.path.join(full_path, pic))
 
 
+def downsample(folder_content, full_path):
+
+    # ----------- DOWNSAMPLING THE PICTURES
+    # separate loop just for debugging reasons
+   
+    for pic in folder_content:
+        #print(f"pic {pic} start")  
+        
+        path_pic = os.path.join(full_path, pic)
+        image = cv2.imread(path_pic)
+        h, w = image.shape [:2]
+        #print( h,w)
+
+        if min(h,w) > 1000:
+            #print(f"Downsampling the image {pic}")
+            downsample_factor = 1000/min(h,w)
+            new_h, new_w = int(h*downsample_factor), int(w*downsample_factor)
+            new_image = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
+            cv2.imwrite(path_pic, new_image)
+    
+
+# -------------- Face Recognition
+    # "pic" ist just the name of the picture
+def recognize_face (folder_content, participant_folder, item):   
+
+    # create an output folder 
+    output_folder = os.path.join (participant_folder, item + "_output")
+    os.makedirs(output_folder, exist_ok=True)
+   
+    
+
+    for i, pic in enumerate(folder_content):
+        #print (f"Picture {pic} processing...")
+
+        full_path = os.path.join(participant_folder,item, pic)
+       
+        image = cv2.imread(full_path)
+        
+        face = face_recognition.face_locations(image, model="cnn")
+        #print(f"Gesichtserkennung abgeschlossen. {len(face)} Gesichter gefunden.")
+
+        # HIER AM BESTEN IN DEBUG FILE SCHREIBEN!!!!!!!!!!!!!!
+        if not face:
+            print(f"Picture {pic} muss manuell verarbeitet werden!")
+        
+        
+        # Jedes erkannte Gesicht ausschneiden und skalieren
+        for (top, right, bottom, left) in face:
+            h, w, _ = image.shape
+            
+            # Abstand um 20% vergrößern
+            padding = int((bottom - top) * 0.3)  
+
+            top = max(0, top - padding)
+            bottom = min(h, bottom + padding)
+            left = max(0, left - padding)
+            right = min(w, right + padding)
+
+            face = image[top:bottom, left:right]
+            face_resized = cv2.resize(face, (400, 400), interpolation=cv2.INTER_LINEAR)
+            #print (os.path.join(output_folder, pic))
+            new_name = f"{item}_{i}.jpg"
+            cv2.imwrite(os.path.join(output_folder, new_name), face_resized)
+        
+
+        
+    
+
+
 
 
 def main():
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from face_detection import align_format
+from face_detection import align_format, downsample, recognize_face
 
 import tkinter as tk
 from tkinter import filedialog
-import cv2
 import os
+import time
 
 # participant_folder  - PATH of participant Folder
 # folder_content - ARRAY with all Data in Participant folder (NOT A PATH!)
@@ -25,12 +25,24 @@ def main():
 # PROCESSING OF SEPARATE FOLDERS (mom, dad etc.) in Participant Folder
     for item in folder_content:
         full_path = os.path.join(participant_folder, item)
+
         if os.path.isdir(full_path):
         
-         print (f'Folder "{item}" found: {full_path}')
-         images = os.listdir(full_path)
+         #print (f'Folder "{item}" found: {full_path}') #debug
+         pictures_of_person = os.listdir(full_path)
 
-         align_format (images, full_path) #must be resaved!
+         start = time.time()   
+        # Apple has .heic .heif format that cannot be proccessed with cv
+        # this funktion translates every .heic (if there are any) into .jpg
+         align_format (pictures_of_person, full_path) 
+         pictures_of_person = os.listdir(full_path)
+
+         print(f'Downsampling Folder "{item}" ...')
+         downsample(pictures_of_person, full_path)
+         recognize_face (pictures_of_person, participant_folder, item)
+         ende = time.time() 
+         print (f"Processing {ende - start} seconds")
+
 
         #print(images) #test the content of the folder
 
